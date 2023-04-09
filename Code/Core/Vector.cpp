6eccc12b9f379ffc134bed7ba3c8dfdcf28fc42b
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-04-09 14:32:08 +0700

    Fix atan2f and sqrtf not available in GCC (MinGW-w64 8.1.0) environment (CodeBlocks 20.03)

--- Code/Core/Vector.cpp
+++ Code/Core/Vector.cpp
@@ -59,13 +59,8 @@ bool Vector::operator!=(Vector const& other) {
     return !(*this == other);
 }
 
-#include <type_traits>
 Scalar Vector::getModule() const {
-	if constexpr (std::is_same_v<Scalar, float>) {
-		return std::sqrtf(x * x + y * y);
-	} else {
-		return std::sqrt(x * x + y * y);
-	}
+    return sqrt(x * x + y * y);
 }
 
 void Vector::rotate(Scalar a) {
@@ -77,11 +72,7 @@ void Vector::rotate(Scalar a) {
 
 Scalar Vector::getAngleOfSlope() const {
 	if (x == 0) return 0; // not really, but suit our purpose.
-	if constexpr (std::is_same_v<Scalar, float>) {
-		return std::atan2f(y, x);
-	} else {
-		return std::atan2(y, x);
-	}
+    return atan2(y, x);
 }
 
 Vector getRotatedVector(Vector v, Scalar const& angle) {
